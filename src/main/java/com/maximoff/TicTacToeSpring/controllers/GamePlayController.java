5b87673b1b41fc30aa51fe8fcb7875be141a8816
MaxMaximmoff <MioMan1997@gmail.com>
2022-03-31 08:49:15 +0300

    Убрал ошибочный комментарий в

--- src/main/java/com/maximoff/TicTacToeSpring/controllers/GamePlayController.java
+++ src/main/java/com/maximoff/TicTacToeSpring/controllers/GamePlayController.java
@@ -9,6 +9,9 @@ import com.maximoff.TicTacToeSpring.models.Step;
 import com.maximoff.TicTacToeSpring.services.GameplayService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.*;
@@ -19,17 +22,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-//@AllArgsConstructor
+@AllArgsConstructor
 @RequestMapping("/gameplay")
 public class GamePlayController {
 
     private final GameplayService gameplayService;
-    private final SimpMessagingTemplate simpMessagingTemplate;
 
-    public GamePlayController(GameplayService gameplayService, SimpMessagingTemplate simpMessagingTemplate) {
-        this.gameplayService = gameplayService;
-        this.simpMessagingTemplate = simpMessagingTemplate;
-    }
+//    private final SimpMessagingTemplate simpMessagingTemplate;
+
+//    public GamePlayController(GameplayService gameplayService, SimpMessagingTemplate simpMessagingTemplate) {
+//        this.gameplayService = gameplayService;
+//        this.simpMessagingTemplate = simpMessagingTemplate;
+//    }
+    
 
     @PostMapping("/start")
     public ResponseEntity<Gameplay> start(@RequestBody Player player) {
@@ -37,34 +42,34 @@ public class GamePlayController {
         return ResponseEntity.ok(gameplayService.createGame(player));
     }
 
-//    @PostMapping("/connect")
-//    public ResponseEntity<Gameplay> connectRandom(@RequestBody Player player) throws NotFoundException, InvalidGameException {
-//        log.info("connect request {}", player);
-//        return ResponseEntity.ok(gameplayService.connectToGame(player));
-//    }
-//
-//    @PostMapping("/game")
-//    public ResponseEntity<Gameplay> gamePlay(@RequestBody Step request) throws NotFoundException, InvalidGameException {
-//        log.info("step request: {}", request);
-//        Gameplay gameplay = gameplayService.makeStep(request);
+    @PostMapping("/connect")
+    public ResponseEntity<Gameplay> connectRandom(@RequestBody Player player) throws NotFoundException, InvalidGameException {
+        log.info("connect request {}", player);
+        return ResponseEntity.ok(gameplayService.connectToGame(player));
+    }
+
+    @PostMapping("/game")
+    public ResponseEntity<Gameplay> gamePlay(@RequestBody Step request) throws NotFoundException, InvalidGameException {
+        log.info("step request: {}", request);
+        Gameplay gameplay = gameplayService.makeStep(request);
 //        simpMessagingTemplate.convertAndSend("/topic/game-progress/", gameplay);
-//        return ResponseEntity.ok(gameplay);
-//    }
-//
-//    @GetMapping("/game")
-//    public ResponseEntity<Gameplay> gamePlay_(@RequestParam("text") String text) throws NotFoundException, InvalidGameException {
-//        log.info("step text request: {}", text);
-//        Gameplay gameplay = gameplayService.getGamplay(text);
+        return ResponseEntity.ok(gameplay);
+    }
+
+    @GetMapping("/game")
+    public ResponseEntity<Gameplay> gamePlay_(@RequestParam("text") String text) throws NotFoundException, InvalidGameException {
+        log.info("step text request: {}", text);
+        Gameplay gameplay = gameplayService.getGamplay(text);
 //        simpMessagingTemplate.convertAndSend("/topic/game-progress/", gameplay);
-//        return ResponseEntity.ok(gameplay);
-//    }
-//
-//    @GetMapping("/playback")
-//    public ResponseEntity<Json> gamePlay1(@RequestParam("json") String json_type) throws NotFoundException, InvalidGameException {
-//        log.info("json request: {}", json_type);
-//        Json json = gameplayService.getJsonStor(json_type);
-//        return ResponseEntity.ok(json);
-//    }
+        return ResponseEntity.ok(gameplay);
+    }
+
+    @GetMapping("/playback")
+    public ResponseEntity<Json> gamePlay1(@RequestParam("json") String json_type) throws NotFoundException, InvalidGameException {
+        log.info("json request: {}", json_type);
+        Json json = gameplayService.getJsonStor(json_type);
+        return ResponseEntity.ok(json);
+    }
 
 
 }
